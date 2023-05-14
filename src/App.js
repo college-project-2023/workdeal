e595f2c832eac5433626463665ad1c7107211bc1
sagar2003 <81001853+sagar2003@users.noreply.github.com>
2023-05-14 13:17:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 
 function App() {
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyBUy2-fbp3_rX5_e5rG39zkI_VH46KuCN8',
-  });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
 
   return (
     <div className="App">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerClassName="map-container"
-          center={center}
-          zoom={10}
-        />
-      )}
+      
     </div>
   );
 }
